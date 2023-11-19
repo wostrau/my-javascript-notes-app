6783d932ea7df8184b5d06e5267e7aeeae983e50
wostrau <a.ostrovtsov@gmail.com>
2023-11-19 23:57:47 +0100

    added info about loops from javascript.info

---
+++ scripts/basics.js
@@ -0,0 +1,22 @@
+//!https://javascript.info/while-for
+
+/*
+loops:
+(1) while
+(2) do ... while
+(3) for (begin, condition, step) body
+(4) inline variable declaration
+(5) skipping parts (omit begin)
+(6) break / continue
+(7) labels for break / continue
+
+Example:
+for (let i = 0; i < 10; i++) {
+    if (i === 0) continue;
+
+    if (!(i % 2)) {
+        alert(i);
+    }
+}
+*/
+
